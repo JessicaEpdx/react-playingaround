b8a7095bf474a1a92cdb7d90c6852d9775cbb3b8
Jessica Engel <student@epicodus.com>
2015-07-28 10:12:54 -0700

    comment component in list, list in holder

--- public/scripts/comments.js
+++ public/scripts/comments.js
@@ -3,11 +3,32 @@ var CommentHolder = React.createClass({
     return(
       <div>
         <p>Comment Holder</p>
+        <CommentList />
       </div>
-
-    )
+    );
   }
-})
+});
+
+var CommentList = React.createClass({
+  render: function() {
+    return(
+      <div>
+        <p>Comment List</p>
+        <Comment />
+      </div>
+    );
+  }
+});
+
+var Comment = React.createClass({
+  render: function() {
+    return(
+      <div>
+        <p>Comment</p>
+      </div>
+    );
+  }
+});
 
 React.render(
   <CommentHolder />,
